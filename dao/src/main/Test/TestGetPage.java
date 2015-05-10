b34fb6b04351bb5bf813105cb3847af65ab60d42
HappyQ <followhappyq@gmail.com>
2015-05-10 04:19:58 +0300

    properties path

---
+++ dao/src/main/Test/TestGetPage.java
@@ -0,0 +1,15 @@
+import dao.Dao;
+
+import dao.MyDao;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TestGetPage {
+    @Test
+    public void TestGetPage(){
+        Dao dao = MyDao.getDao();
+        dao.getPage("main");
+        assertNotNull("Users not null", dao.getPage("main"));
+    }
+}
--- dao/src/main/java/dao/Dao.java
+++ dao/src/main/java/dao/Dao.java
@@ -18,4 +18,5 @@ public interface Dao {
     Users getUser(int idu);
     List<Users> getUsers();
 
+
 }
