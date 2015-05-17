91d82e880c8122fd8bbde36cda700765c9750f5a
ragexe <ragexe@mail.ru>
2015-05-17 22:39:21 +0300

    AdminFilter

--- services/src/main/java/admin/AdminFilter.java
+++ services/src/main/java/admin/AdminFilter.java
@@ -4,7 +4,6 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class AdminFilter implements Filter{
 
@@ -13,14 +12,10 @@ public class AdminFilter implements Filter{
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         resp.setContentType("text/html;charset=UTF-8");
         req.setCharacterEncoding("UTF-8");
-        PrintWriter out = resp.getWriter();
         HttpSession session = ((HttpServletRequest) req).getSession();
-        if (session.getAttribute("role").toString().equals("1")){
+        if (!"null".equals(session) && session.getAttribute("role").toString().equals("1")) {
             chain.doFilter(req, resp);
-        }else{
-//            out.println("username or password error!");
-//            out.println(session.getAttribute("role").toString());
-//            out.println("/login.jsp");
+        } else {
             RequestDispatcher rd = req.getRequestDispatcher("login.jsp");
             rd.include(req, resp);
         }
