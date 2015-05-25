3ad2baf6d4a6db2fc450143eccad8de3f46c202b
ragexe <ragexe@mail.ru>
2015-05-25 17:51:46 +0300

    entities module

--- dao/src/main/Test/TestGetPage.java
+++ dao/src/main/Test/TestGetPage.java
@@ -10,6 +10,6 @@ public class TestGetPage {
     public void TestGetPage(){
         Dao dao = MyDao.getDao();
         dao.getPage("main");
-        assertNotNull("Page not null", dao.getPage("main"));
+        assertNotNull("Page not null", dao.getPage("main"));//
     }
 }
