cc2c9e892e6b3b39a66bf81fef5c78ea1ef94608
happyq <followhappyq@gmail.com>
2015-06-16 13:13:47 +0300

    pagination

---
+++ web/src/main/java/util/Pagination.java
@@ -0,0 +1,23 @@
+package util;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by HappyQ on 16.06.2015.
+ */
+public class Pagination extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        doGet(request, response);
+    }
+
+
+}
