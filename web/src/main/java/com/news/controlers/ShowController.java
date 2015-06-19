5521c24980f57beb758cc2e275fbb5e374586810
Alexander Deshkevich <dread@hightorquesoftware.com>
2015-06-20 00:25:43 +0300

    родила мама дебила

--- web/src/main/java/com/news/controlers/ShowController.java
+++ web/src/main/java/com/news/controlers/ShowController.java
@@ -1,5 +1,7 @@
 package com.news.controlers;
 
+import data.Category;
+import data.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -7,17 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import util.CategoryService;
+import util.PageService;
+
+import java.util.List;
 
 @Controller
 public class ShowController {
 
     @Autowired
     CategoryService categoryService;
+    private List<Page> pages;
+    private PageService pageService;
 
-    @RequestMapping(value = "/test/hello", method = RequestMethod.GET)
-    public String helloTest(ModelAndView model) {
-        model.addAttribute("category","");
+    @RequestMapping(value = "/test1", method = RequestMethod.GET)
+    public String helloTest(ModelMap model) {
+        List<Category> categories = categoryService.getList();
+        model.addAttribute("category",categories);
 
-        return "main";
+        return "cat";
     }
 }
