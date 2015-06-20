546789c98a149bbab8a3a94087cd936b52b0c9ea
ragexe <ragexe@mail.ru>
2015-06-20 16:29:12 +0300

    Comment entity

---
+++ entities/src/main/java/data/Comment.java
@@ -0,0 +1,106 @@
+package data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+* Created by ragexe on 27.05.2015.
+*/
+@Entity
+@Table(name = "t_comment", schema = "", catalog = "newsbase")
+public class Comment extends CustomEntity implements Serializable {
+    private static final long serialVersionUID = -373183010734040400L;
+
+    @Id
+    @Column(name = "F_COMMENT_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "F_PAGE_ID", nullable = false)
+    private Page page;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "F_USER_ID", nullable = false)
+    private User user;
+
+    @Basic
+    @Column(name = "F_TEXT", nullable = false, insertable = true, updatable = true, length = 65535)
+    private String maintext;
+
+    @Basic
+    @Column(name = "F_DATE", nullable = false, insertable = true, updatable = true)
+    private java.sql.Date date;
+
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Page getPage() {
+        return page;
+    }
+
+    public void setPage(Page page) {
+        this.page = page;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getMaintext() {
+        return maintext;
+    }
+
+    public void setMaintext(String maintext) {
+        this.maintext = maintext;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment)) return false;
+
+        Comment comment = (Comment) o;
+
+        if (id != comment.id) return false;
+        if (!date.equals(comment.date)) return false;
+        if (!maintext.equals(comment.maintext)) return false;
+        if (!page.equals(comment.page)) return false;
+        if (!user.equals(comment.user)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + page.hashCode();
+        result = 31 * result + user.hashCode();
+        result = 31 * result + maintext.hashCode();
+        result = 31 * result + date.hashCode();
+        return result;
+    }
+}
