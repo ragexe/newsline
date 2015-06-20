c64cdf36dfbfe8998255f7dbe60dd70980970433
ragexe <ragexe@mail.ru>
2015-06-20 17:36:51 +0300

    Comment Service

--- services/src/main/java/by/newsline/service/CommentService.java
+++ services/src/main/java/by/newsline/service/CommentService.java
@@ -1,11 +1,8 @@
 package by.newsline.service;
 
-
-import by.newsline.dao.ICategoryDao;
 import by.newsline.dao.ICommentDao;
 import by.newsline.dao.util.exception.DaoException;
 import by.newsline.service.util.exception.ServiceException;
-import data.Category;
 import data.Comment;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-//import by.newsline.dao.util.exception.PersistException;
+
 
 /**
  * Created by HappyQ on 10.06.2015.
@@ -28,41 +25,6 @@ public class CommentService implements ICommentService {
     @Autowired
     private ICommentDao commentDao;
 
-    //    public void saveCategory(Category comment) throws ServiceException{
-//        try {
-//            commentDao.saveCategory(comment);
-//        } catch (DaoException e) {
-//            logger.error(e.getMessage());
-//            throw new ServiceException(e);
-//        }
-//    }
-//
-//    public void deleteCategoryById(long id) throws ServiceException {
-//        try {
-//            commentDao.deleteCommentById(id);
-//        } catch (DaoException e) {
-//            logger.error(e.getMessage());
-//            throw new ServiceException(e);
-//        }
-//    }
-//
-//    public Category getById(long id) throws ServiceException{
-//        try {
-//            return commentDao.getById(id);
-//        } catch (DaoException e) {
-//            logger.error(e.getMessage());
-//            throw new ServiceException();
-//        }
-//    }
-//
-//    public List<Category> getAllCategories() throws ServiceException{
-//        try {
-//            return commentDao.getAllCategories();
-//        } catch (DaoException e) {
-//            logger.error(e.getMessage());
-//            throw new ServiceException();
-//        }
-//    }
     public void saveComment(Comment comment)throws ServiceException{
         try {
             commentDao.saveComment(comment);
