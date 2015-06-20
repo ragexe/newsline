e75cd05196d84a49e05f9b93cf9178e0e272080c
Alexander Deshkevich <deshkevich.alexander@gmail.com>
2015-06-20 19:49:12 +0300

    footer dl9 dolbaebov

--- web/src/main/webapp/asserts/js/custom.js
+++ web/src/main/webapp/asserts/js/custom.js
@@ -1,8 +1,14 @@
 $( document ).ready(function() {
+    var docHeight = $('body').height() + 100;
     function setFooter(){
-        if($(document).height() <= $(window).height()){
+        if(docHeight <= $(window).height()){
             $('.custom_footer').css("top", $(window).height()-80);
         }
     }
     setFooter();
-})();
+    $( window ).resize(function() {
+        if(docHeight <= $(window).height()){
+            $('.custom_footer').css("top", $(window).height()-80);
+        }
+    });
+});
